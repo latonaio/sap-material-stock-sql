880014c257253f542b46a0c56bc74856375d45a3
Yurino Oga <yurino.o@latona.io>
2021-12-24 17:51:53 +0900

    release

--- sap-material-stock-sql.sql
+++ sap-material-stock-sql.sql
@@ -1,11 +1,9 @@
 CREATE TABLE `sap-material-stock`
 (
-    `Material`              varchar(40) DEFAULT NULL,
-    `Plant`                 varchar(4) DEFAULT NULL,
-    `StorageLocation`       varchar(4) DEFAULT NULL,
-    `Batch`                 varchar(10) DEFAULT NULL,
-    `MatlWrhsStkQtyInMatlBaseUnit` varchar(13) DEFAULT NULL,
-    `to_MaterialStock`      varchar(13) DEFAULT NULL,
+    `Material`              varchar(40) NOT NULL,
+    `Plant`                 varchar(4) NOT NULL,
+    `StorageLocation`       varchar(4) NOT NULL,
+    `Batch`                 varchar(10) NOT NULL,
     `Supplier`              varchar(10) DEFAULT NULL,
     `Customer`              varchar(10) DEFAULT NULL,
     `WBSElementInternalID`  varchar(24) DEFAULT NULL,
@@ -14,6 +12,8 @@ CREATE TABLE `sap-material-stock`
     `InventorySpecialStockType` varchar(2) DEFAULT NULL,
     `InventoryStockType`    varchar(2) DEFAULT NULL,
     `MaterialBaseUnit`      varchar(3) DEFAULT NULL,
+    `MatlWrhsStkQtyInMatlBaseUnit` varchar(13) DEFAULT NULL,
+    `to_MaterialStock`      varchar(13) DEFAULT NULL,
     PRIMARY KEY (`Material`, `Plant`, `StorageLocation`, `Batch`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
