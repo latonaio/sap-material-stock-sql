d59c6575c8da04ff5db9320ae008f8ac1da102bd
Yurino Oga <yurino.o@latona.io>
2022-01-25 10:19:56 +0900

    RELEASE

--- sap-material-stock-sql.sql
+++ sap-material-stock-sql.sql
@@ -1,19 +1,18 @@
 CREATE TABLE `sap_material_stock`
 (
-    `Material`              varchar(40) NOT NULL,
-    `Plant`                 varchar(4) NOT NULL,
-    `StorageLocation`       varchar(4) NOT NULL,
-    `Batch`                 varchar(10) NOT NULL,
-    `Supplier`              varchar(10) DEFAULT NULL,
-    `Customer`              varchar(10) DEFAULT NULL,
-    `WBSElementInternalID`  varchar(24) DEFAULT NULL,
-    `SDDocument`            varchar(10) DEFAULT NULL,
-    `SDDocumentItem`        varchar(4) DEFAULT NULL,
-    `InventorySpecialStockType` varchar(2) DEFAULT NULL,
-    `InventoryStockType`    varchar(2) DEFAULT NULL,
-    `MaterialBaseUnit`      varchar(3) DEFAULT NULL,
+    `Material`                     varchar(40) NOT NULL,
+    `Plant`                        varchar(4) NOT NULL,
+    `StorageLocation`              varchar(4) NOT NULL,
+    `Batch`                        varchar(10) NOT NULL,
+    `Supplier`                     varchar(10) NOT NULL,
+    `Customer`                     varchar(10) NOT NULL,
+    `WBSElementInternalID`         varchar(24) NOT NULL,
+    `SDDocument`                   varchar(10) NOT NULL,
+    `SDDocumentItem`               varchar(4) NOT NULL,
+    `InventorySpecialStockType`    varchar(2) NOT NULL,
+    `InventoryStockType`           varchar(2) NOT NULL,
+    `MaterialBaseUnit`             varchar(3) DEFAULT NULL,
     `MatlWrhsStkQtyInMatlBaseUnit` varchar(13) DEFAULT NULL,
-    `to_MaterialStock`      varchar(13) DEFAULT NULL,
-    PRIMARY KEY (`Material`, `Plant`, `StorageLocation`, `Batch`)
+    PRIMARY KEY (`Material`, `Plant`, `StorageLocation`, `Batch`, `Supplier`, `Customer`, `WBSElementInternalID`, `SDDocument`, `SDDocumentItem`, `InventorySpecialStockType`, `InventoryStockType`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
