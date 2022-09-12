7a065a822513f44b11d4b1decdd8e4ed4b19cd9e
Yurino Oga <yurino.o@latona.io>
2022-09-13 00:29:21 +0900

    release

--- sap-material-stock-sql.sql
+++ sap-material-stock-sql.sql
@@ -1,18 +1,18 @@
-CREATE TABLE `sap_material_stock`
+CREATE TABLE `sap_material_stock_data`
 (
     `Material`                     varchar(40) NOT NULL,
     `Plant`                        varchar(4) NOT NULL,
     `StorageLocation`              varchar(4) NOT NULL,
-    `Batch`                        varchar(10) NOT NULL,
-    `Supplier`                     varchar(10) NOT NULL,
-    `Customer`                     varchar(10) NOT NULL,
-    `WBSElementInternalID`         varchar(24) NOT NULL,
-    `SDDocument`                   varchar(10) NOT NULL,
-    `SDDocumentItem`               varchar(4) NOT NULL,
-    `InventorySpecialStockType`    varchar(2) NOT NULL,
-    `InventoryStockType`           varchar(2) NOT NULL,
+    `Batch`                        varchar(10) DEFAULT NULL,
+    `Supplier`                     varchar(10) DEFAULT NULL,
+    `Customer`                     varchar(10) DEFAULT NULL,
+    `WBSElementInternalID`         varchar(24) DEFAULT NULL,
+    `SDDocument`                   varchar(10) DEFAULT NULL,
+    `SDDocumentItem`               varchar(4) DEFAULT NULL,
+    `InventorySpecialStockType`    varchar(2) DEFAULT NULL,
+    `InventoryStockType`           varchar(2) DEFAULT NULL,
     `MaterialBaseUnit`             varchar(3) DEFAULT NULL,
     `MatlWrhsStkQtyInMatlBaseUnit` varchar(13) DEFAULT NULL,
-    PRIMARY KEY (`Material`, `Plant`, `StorageLocation`, `Batch`, `Supplier`, `Customer`, `WBSElementInternalID`, `SDDocument`, `SDDocumentItem`, `InventorySpecialStockType`, `InventoryStockType`)
+    PRIMARY KEY (`Material`, `Plant`, `StorageLocation`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
